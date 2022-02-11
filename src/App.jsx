42a86d8f79cad0c708a0e4f6f170a19af3537649
Michael <mlee85@gmail.com>
2022-02-10 19:45:23 -0800

    Adding Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import Authentication from './components/Authentication/Authentication.jsx';
 import Profile from './components/User_Profile/Profile.jsx';
 import SearchBar from './components/Search/SearchBar.jsx';
+import { useAuth0 } from '@auth0/auth0-react';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  LoginButton () {
+    const { loginWithRedirect } = useAuth0();
   }
 
   render () {
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Auth0Provider
+    domain=""
+    clientId=""
+    redirectUri={window.location.origin}
+  >
+    <App/>
+  </Auth0Provider>,
+  document.getElementById('app')
+);
