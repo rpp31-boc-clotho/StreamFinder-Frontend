84a749d5bacb4edc88a9d821a83e314e595f6a7e
Michael <mlee85@gmail.com>
2022-02-12 00:28:13 -0800

    Added MaterialUI

--- src/components/Landing/LoginButton.jsx
+++ src/components/Landing/LoginButton.jsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 const LoginButton = () => {
@@ -8,12 +11,67 @@ const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   const { isAuthenticated } = useAuth0();
   const { logout } = useAuth0();
+  const { user } = useAuth0();
 
+  let currentUser;
+
+  // if (isAuthenticated) {
+  //   return <button onClick={() => logout()}>Log Out</button>
+  // } else {
+  //   return <button onClick={() => loginWithRedirect()}>Log In</button>
+  // }
   if (isAuthenticated) {
-    return <button onClick={() => logout()}>Log Out</button>
+    currentUser = user.name;
   } else {
-    return <button onClick={() => loginWithRedirect()}>Log In</button>
+    currentUser = 'Log In'
   }
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  if (currentUser === 'Log In') {
+    return <button onClick={() => loginWithRedirect()}>Log In</button>
+  } else {
+    return (
+      <div>
+        <Button
+          id="demo-positioned-button"
+          aria-controls={open ? 'demo-positioned-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+        >
+          {currentUser}
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={() => logout()}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
+
+  }
+
 };
 
 export default LoginButton;
