57cd416e98fc83854da0bd0799e9b9634d9c8de1
Michael <mlee85@gmail.com>
2022-02-11 23:17:56 -0800

    Removed config.js reference

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
-import auth0_domain from '../config.js';
-import auth0_clientId from '../config.js';
 
 ReactDOM.render(
   <Auth0Provider
