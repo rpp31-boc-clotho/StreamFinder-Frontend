8945ca4c17a89be312e26135bec7cc29d133ec37
Michael <mlee85@gmail.com>
2022-02-12 01:02:35 -0800

    Additional auth code cleanup

--- src/components/Landing/LoginButton.jsx
+++ src/components/Landing/LoginButton.jsx
@@ -8,9 +8,7 @@ import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import PersonAdd from '@mui/icons-material/PersonAdd';
 import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
 
@@ -45,7 +43,6 @@ const LoginButton = () => {
     return (
       <React.Fragment>
         <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-
           <Tooltip title="Account settings">
             <IconButton
               onClick={handleClick}
@@ -55,7 +52,7 @@ const LoginButton = () => {
               aria-haspopup="true"
               aria-expanded={open ? 'true' : undefined}
             >
-              <Avatar sx={{ width: 32, height: 32 }}>{currentUser[0]}</Avatar>
+              <Avatar sx={{ width: 32, height: 32 }}>{currentUser[0].toUpperCase()}</Avatar>
             </IconButton>
           </Tooltip>
         </Box>
