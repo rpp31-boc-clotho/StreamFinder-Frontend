e85a2209d4a122589db14c1cfaeaf4bde4c0e535
Adam <adam.a.lohnes@gmail.com>
2022-02-16 09:03:54 -0800

    init media info page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let PORT = 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
