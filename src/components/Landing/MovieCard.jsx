fb3137fef4661cbfab1fb7bfaa1f73cbafffcd18
Sangeetha Nair <sangeetha.prasanth@gmail.com>
2022-02-24 14:28:36 -0800

    added error component and call to server to get data for landing Page

--- src/components/Landing/MovieCard.jsx
+++ src/components/Landing/MovieCard.jsx
@@ -10,7 +10,8 @@ class MovieCard extends React.Component {
   }
 
   displayDate () {
-    if (this.props.film.media_type === 'movie') {
+    //added one more condition to make it work for data from backend
+    if (this.props.film.media_type === 'movie' ||  this.props.film.mediaType === 'movie') {
       return (this.props.film.release_date.substring(0,4))
     } else {
       return (this.props.film.first_air_date.substring(0,4))
@@ -18,7 +19,15 @@ class MovieCard extends React.Component {
   }
 
   displayImage () {
-    let url = ('https://image.tmdb.org/t/p/w185' + (this.props.film.poster_path))
+    //added for backend data
+    let url ="";
+    if(this.props.film.poster_path) {
+      url = ('https://image.tmdb.org/t/p/w185' + (this.props.film.poster_path))
+    } else {
+      url = this.props.film.imgUrl;
+    }
+    //let url = ('https://image.tmdb.org/t/p/w185' + (this.props.film.poster_path))
+
     return (<img src={url} className="cardImage"></img>)
   }
 
@@ -27,7 +36,8 @@ class MovieCard extends React.Component {
       <div className="filmCard">
         <section className="filmImage"> {this.displayImage()} </section>
         <section className="filmRating">
-          {this.getRating(this.props.film.vote_average)}
+          {this.getRating(this.props.film.rating)}
+
           </section>
         <section className="filmTitle">
           {this.props.film.name || this.props.film.title}
@@ -41,3 +51,5 @@ class MovieCard extends React.Component {
 }
 
 export default MovieCard;
+//removed for backend data
+//{this.getRating(this.props.film.vote_average)}
