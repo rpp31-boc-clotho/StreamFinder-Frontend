cbf1e572f628d917b8318ba75f69e4b798d42310
MPollens2015 <maapollens@gmail.com>
2022-03-01 20:09:56 -0500

    MovieCard code cleanup

--- src/components/Landing/MovieCard.jsx
+++ src/components/Landing/MovieCard.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import QuestionMark from '/public/SeekPng.com_questionmark-png_1144413.png';
 
 class MovieCard extends React.Component {
   constructor(props) {
     super(props)
   }
 
-
-
   displayRating(rating) {
-    console.log("Rating - "+rating)
+    //accounted for ratings of 0 in the render function
     let ratingPercent = (rating * 10)
-    console.log("Rating - perrcent "+ratingPercent)
     if (ratingPercent >= 75) {
       return  <section className="filmRating" style={{ background: 'rgb(14, 179, 14)' }}>{ratingPercent + '%'}</section>
     } else if (ratingPercent <= 74 && ratingPercent >= 50) {
@@ -29,13 +25,10 @@ class MovieCard extends React.Component {
         return (this.props.film.release_date.substring(0,4))
       }
     }
-    // else {
-    //   return (this.props.film.first_air_date.substring(0,4))
-    // }
   }
 
   displayImage () {
-    //added for backend data
+    //added for backend data and accounted for brooken links
     let url ="";
     if(this.props.film.poster_path) {
       url = ('https://image.tmdb.org/t/p/w185' + (this.props.film.poster_path))
@@ -46,7 +39,6 @@ class MovieCard extends React.Component {
         url = this.props.film.imgUrl;
       }
     }
-    //let url = ('https://image.tmdb.org/t/p/w185' + (this.props.film.poster_path))
 
     return (<img src={url} className="cardImage"></img>)
   }
@@ -80,4 +72,3 @@ class MovieCard extends React.Component {
 }
 
 export default MovieCard;
-
