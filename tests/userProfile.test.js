79c81c018fb88db254e4656b0bae19f1c83dbc35
Michael <mlee85@gmail.com>
2022-03-01 20:06:27 -0800

    Set up jest and got react router testing to pass

--- tests/userProfile.test.js
+++ tests/userProfile.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
+import {render, screen} from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import {MemoryRouter} from 'react-router-dom';
+import {createMemoryHistory} from 'history'
+import { useNavigate } from 'react-router-dom';
+import {BrowserRouter, Router} from 'react-router-dom';
 
 import Profile from '../src/components/User_Profile/Profile.jsx';
 import Providers from '../src/components/User_Profile/Providers.jsx';
@@ -23,18 +28,27 @@ afterEach(() => {
 });
 
 describe('Rendering Components', () => {
-  test('renders Profile Component', () => {
+  test('renders Subscriptions Component', () => {
     act(() => {
-      render(<Profile
-              isLoggedIn={true}
-              providersList={testData.userData.userProfile.subscriptions}
-              username={'email12345'}
-              email={'email12345@gmail.com'}
-              recentlyWatched={[]}
-              avatar={'/sflogo2.png'}
-              updateSubscriptions={[]}
-            />, container);
+      render(
+        <Subscriptions providersList={testData.userData.userProfile.subscriptions}/>, container)
     });
-    const component = document.getElementsByClassName('module_container');
+
+    const component = document.getElementsByClassName('subscribedProviders');
     expect(component.length).toBe(1);
   });
+})
+
+const renderWithRouter = (ui, {route = '/'} = {}) => {
+  window.history.pushState({}, 'Test page', route)
+
+  return render(ui, {wrapper: BrowserRouter})
+}
+
+test('rendering Profile Component', () => {
+  const route = '/settings'
+  renderWithRouter(<Profile />, {route})
+
+  expect(screen.getByTestId('profile-test')).toBeTruthy()
+})
+
