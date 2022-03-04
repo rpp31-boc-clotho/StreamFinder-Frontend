88d099920e7d0b9b711bbb93e11bb3ddcb512a19
MPollens2015 <maapollens@gmail.com>
2022-03-03 21:09:16 -0500

    minor url update to searchFunction

--- src/components/Search/SearchFunction.js
+++ src/components/Search/SearchFunction.js
@@ -3,7 +3,7 @@ const axios = require("axios").default
 let searchFunction = (mediaType, searchTerm) => {
   let searched = {
     method: 'GET',
-    url: `http://boc-backend-alb-1007494829.us-east-2.elb.amazonaws.com/homepage/search/${mediaType}?media=${searchTerm}`,
+    url: `https://api.youpostalservice.com/homepage/search/${mediaType}?media=${searchTerm}`,
     headers: {}
   }
   return axios.request(searched)
