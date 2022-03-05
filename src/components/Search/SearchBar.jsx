7fe60d84f7fa24cf373179af9d2454df9d03326b
Adam <adam.a.lohnes@gmail.com>
2022-03-04 19:13:00 -0800

    style landing page search > logged in

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -11,16 +11,23 @@ function searchBar({handleSearch}) {
  return (
     <div className="searchDiv">
       <form action="/search" onSubmit={onSearch} onChange={changeTerm}>
-      <label htmlFor="Search_Bar">
-          <span className="Search_Bar_Title">Search for Movies or Shows!</span>
-      </label>
-        <input
-          type="text"
-          id="Search_Bar"
-          placeholder="Search for Movies or Shows!"
-          name="SB"
-        />
-        <button type="submit" id="searchSubmit">Search</button>
+        <label htmlFor="Search_Bar">
+            <span className="Search_Bar_Title">What do you want to watch?</span>
+        </label>
+        <div className="search-container">
+          <input
+            type="text"
+            id="Search_Bar"
+            placeholder="Search for Movies or Shows!"
+            name="SB"
+          />
+          <button type="submit" id="searchSubmit">
+            <svg width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="18" cy="11" r="8.5" stroke="#353531" stroke-width="5"/>
+              <line x1="10.3383" y1="16.1907" x2="4.19072" y2="20.6617" stroke="#353531" stroke-width="6" stroke-linecap="round"/>
+            </svg>
+          </button>
+        </div>
       </form>
     </div>
   )
